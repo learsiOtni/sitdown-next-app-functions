349cdd43d12cd371e7ed9d6e8c0e72677646ca15
learsiOtni <108980854+learsiOtni@users.noreply.github.com>
2023-08-17 23:41:20 +0100

    Delete config.js

--- util/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    apiKey: "AIzaSyCrzT31PG1lNhCAkFGTD3spmRwExhCqSH4",
-    authDomain: "sitdown-834fc.firebaseapp.com",
-    projectId: "sitdown-834fc",
-    storageBucket: "sitdown-834fc.appspot.com",
-    messagingSenderId: "466838025867",
-    appId: "1:466838025867:web:4365a7b1d318125638068e",
-    measurementId: "G-LXF7KDWN9G"
-}
